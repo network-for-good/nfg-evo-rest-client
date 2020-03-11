dcf4979aac43e9b560e32a9e47d2d2b063fa6c3f
Tom Hoen <tom@givecorps.com>
2020-03-11 12:15:21 -0400

    Preliminary commit

---
+++ lib/nfg/evo/rest/client.rb
@@ -0,0 +1,12 @@
+require "nfg/evo/rest/client/version"
+
+module Nfg
+  module Evo
+    module Rest
+      module Client
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/nfg/evo/rest/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Nfg::Evo::Rest::Client do
+  it "has a version number" do
+    expect(Nfg::Evo::Rest::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
