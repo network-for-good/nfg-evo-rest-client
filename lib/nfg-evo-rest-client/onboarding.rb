dad18e01a9f1344de4f525dd2c4bd1442bbf9e68
Tom Hoen <tom@givecorps.com>
2020-03-11 14:33:24 -0400

    adds the onboarding and product models

---
+++ lib/nfg-evo-rest-client/onboarding.rb
@@ -0,0 +1,22 @@
+module NfgEvoRestClient
+  class Onboarding < Base
+    # can't get the value set in the initializer to populate
+    # the base_url as the class is being loaded before the initializer
+    # is run.
+    base_url SsoOpenid::Urls.evo.fqdn
+
+    before_request do |name, request|
+      request.get_params[:user_email] = nfg_evo_rest_user_email
+      request.get_params[:user_token] = nfg_evo_rest_user_token
+      append_param_fields_if_any(request)
+    end
+
+    post :onboarding, "/api/v1/onboarding/initial_setup"
+
+    private
+
+    def param_fields
+      []
+    end
+  end
+end
