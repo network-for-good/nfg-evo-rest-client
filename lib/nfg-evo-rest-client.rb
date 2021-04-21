12957f42f9a5bd98ccd006a093462ff90739b13b
Mike Subelsky <subelsky@users.noreply.github.com>
2021-04-21 13:23:25 -0400

    minor rubocop style fixes

--- lib/nfg-evo-rest-client.rb
+++ lib/nfg-evo-rest-client.rb
@@ -1,17 +1,23 @@
-require "nfg-evo-rest-client/version"
-require "nfg-evo-rest-client/configuration"
-require "nfg-evo-rest-client/base"
-require "nfg-evo-rest-client/admin"
-require "nfg-evo-rest-client/donation"
-require "nfg-evo-rest-client/donor"
-require "nfg-evo-rest-client/entity"
-require "nfg-evo-rest-client/project"
-require "nfg-evo-rest-client/ticket_purchase"
-require "nfg-evo-rest-client/onboarding"
-require "nfg-evo-rest-client/product"
-require "nfg-evo-rest-client/questionnaires/questionnaire"
-require "nfg-evo-rest-client/questionnaires/response"
+# frozen_string_literal: true
 
+require 'nfg-evo-rest-client/version'
+require 'nfg-evo-rest-client/configuration'
+require 'nfg-evo-rest-client/base'
+require 'nfg-evo-rest-client/admin'
+require 'nfg-evo-rest-client/campaign'
+require 'nfg-evo-rest-client/donation'
+require 'nfg-evo-rest-client/donor'
+require 'nfg-evo-rest-client/entity'
+require 'nfg-evo-rest-client/project'
+require 'nfg-evo-rest-client/ticket_purchase'
+require 'nfg-evo-rest-client/onboarding'
+require 'nfg-evo-rest-client/product'
+require 'nfg-evo-rest-client/questionnaires/questionnaire'
+require 'nfg-evo-rest-client/questionnaires/response'
+
+# Provides a wrapper to the EVO/Fundraising Pages API
+# @see https://github.com/network-for-good/Givecorps-site/tree/develop/api
 module NfgEvoRestClient
+  # General exception class for the gem
   class Error < StandardError; end
 end
