692be87af0363a75394a8b6ed5402df8114513e5
aravindpudayoor <aravind@clearstack.io>
2024-09-13 19:01:19 +0530

    bumps version

--- lib/nfg-evo-rest-client/sponsorship_purchase.rb
+++ lib/nfg-evo-rest-client/sponsorship_purchase.rb
@@ -4,7 +4,7 @@ module NfgEvoRestClient
   class SponsorshipPurchase < Base
     get :all, '/api/v1/sponsorship_purchases'
     get :find, '/api/v1/sponsorship_purchases/:id'
-    post  :edited_by_admin, "/api/v1/sponsorship_purchases/:id/edited_by_admin",  timeout: 60
+    post :edited_by_admin, "/api/v1/sponsorship_purchases/:id/edited_by_admin",  timeout: 60
 
     private
 
