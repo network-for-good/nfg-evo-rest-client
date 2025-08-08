8ad50b5d44a2e06ad99de7648c36e0ad5bbdecea
vishal <c-vishal.verma@bonterratech.com>
2025-08-08 12:09:01 +0530

    rename endpoint method name

--- lib/nfg-evo-rest-client/donor_trend.rb
+++ lib/nfg-evo-rest-client/donor_trend.rb
@@ -1,6 +1,6 @@
 module NfgEvoRestClient
   class DonorTrend < Base
-    get :index, "/api/v1/donor_trends", timeout: 60,
+    get :all, "/api/v1/donor_trends", timeout: 60
     post :mark_synced, "/api/v1/donor_trends/mark_synced", timeout: 60
 
     def param_fields
